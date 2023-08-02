93e5f544347d25abbb711b985a7688369e65ed45
candela99 <gonzalezcandela759@gmail.com>
2023-08-02 10:31:39 -0300

    add password encoder

--- src/main/java/com/example/obspringsecurity/WebSecurityConfig.java
+++ src/main/java/com/example/obspringsecurity/WebSecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.firewall.HttpFirewall;
@@ -45,12 +47,15 @@ public class WebSecurityConfig {
      * */
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
+        UserDetails user = User.withUsername("user")
+                .password(passwordEncoder().encode("password"))
                 .roles("USER","ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(user);
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
